993265aed083d415c9ccd8e32999c04af7579e6f
jcbraz <josecbraz@outlook.com>
2023-02-19 19:59:37 +0000

    Add struct and parsing function

--- src/generator.cpp
+++ src/generator.cpp
@@ -1,15 +1,41 @@
+#include <cstring>
 #include <iostream>
 #include <string>
+#include <vector>
 
-std::string receiveInput(std::string &input) {
-    std::cout << "Enter a string: ";
-    std::cin >> input;
-    return input;
+struct Input {
+    std::string graphicType;
+    int radious;
+    int slices;
+    int stacks;
+    std::string outputFile;
+};
+
+Input parseInput(char commmands[]) {
+    char *parsedInput = std::strtok(commmands, " ");
+    std::vector<std::string> parsedInputs;
+    while (parsedInput != nullptr) {
+        parsedInputs.push_back(parsedInput);
+        parsedInput = std::strtok(nullptr, " ");
+    }
+
+    struct Input instructions_recieved;
+    instructions_recieved.graphicType = parsedInputs[1];
+    instructions_recieved.radious = std::stoi(parsedInputs[2]);
+    instructions_recieved.slices = std::stoi(parsedInputs[3]);
+    instructions_recieved.stacks = std::stoi(parsedInputs[4]);
+    instructions_recieved.outputFile = parsedInputs[5];
+
+    return instructions_recieved;
 }
 
 int main() {
-    std::string input;
-    receiveInput(input);
-    std::cout << "You entered: " << input << std::endl;
+    char commands[] = "generator sphere 1 10 10 sphere.3d";
+    Input input = parseInput(commands);
+    std::cout << "Graphic type: " << input.graphicType << '\n';
+    std::cout << "Radius: " << input.radious << '\n';
+    std::cout << "Slices: " << input.slices << '\n';
+    std::cout << "Stacks: " << input.stacks << '\n';
+    std::cout << "Output file: " << input.outputFile << '\n';
     return 0;
 }
