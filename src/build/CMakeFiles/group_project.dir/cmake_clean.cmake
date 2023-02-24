9639eda5e0d2bd2747ef13144304925c6aeffb33
jcbraz <josecbraz@outlook.com>
2023-02-24 23:18:49 +0000

    Some changes to the parsing methods

---
+++ src/build/CMakeFiles/group_project.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/group_project.dir/generator.cpp.o"
+  "CMakeFiles/group_project.dir/generator.cpp.o.d"
+  "group_project"
+  "group_project.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/group_project.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
