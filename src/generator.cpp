ca3fcdc59eab0d502fcbdf58695578fa14829410
Dud4s <eduardosilvavsc11@gmail.com>
2023-03-01 23:45:57 +0000

    Input parsing to struct done

--- src/generator.cpp
+++ src/generator.cpp
@@ -31,6 +31,7 @@ struct GeometricFigure {
 	GeometricType geometric_type;
 	vector<int> values;
 	double radius;
+
 	public:
 	GeometricFigure() {
 		geometric_type = NONE;
@@ -47,7 +48,7 @@ struct GeometricFigure {
 	}
 };
 
-GeometricType get_geometric_type(string text) {
+GeometricType getGeometricType(string text) {
 	if (text.compare("plane.3d") == 0) return PLANE;
 	else if (text.compare("box.3d") == 0) return BOX;
 	else if (text.compare("sphere.3d") == 0) return SPHERE;
@@ -55,7 +56,7 @@ GeometricType get_geometric_type(string text) {
 	throw invalid_argument("Invalid Geometric Figure!");
 }
 
-void parse_values(GeometricFigure * gf, size_t n, char ** values) {
+void parseValues(GeometricFigure * gf, size_t n, char ** values) {
 	try {
 		gf->values.push_back(stoi(values[n--]));
 		gf->values.push_back(stoi(values[n--]));
@@ -67,41 +68,8 @@ void parse_values(GeometricFigure * gf, size_t n, char ** values) {
 		throw invalid_argument("Invalid Input Values!");
 	}
 }
+
 /*
-
-struct GeometricFigure {
-    string graphicType;
-    double radius_or_length;
-    int slices_or_grid;
-    int stacks;
-    string outputFile;
-};
-
-GeometricFigure parseThreeCoordenateInput(string commands) {
-    stringstream input(commands);
-    string token;
-    vector<string> tokens;
-    while (input >> token) {
-        tokens.push_back(token);
-    }
-
-    GeometricFigure input_struct;
-    if (tokens.size() == 6) {
-        input_struct.graphicType = tokens[1];
-        input_struct.radius_or_length = stod(tokens[2]);
-        input_struct.slices_or_grid = stoi(tokens[3]);
-        input_struct.stacks = stoi(tokens[4]);
-        input_struct.outputFile = tokens[5];
-    } else if (tokens.size() == 5) {
-        input_struct.graphicType = tokens[1];
-        input_struct.radius_or_length = stod(tokens[2]);
-        input_struct.slices_or_grid = stoi(tokens[3]);
-        input_struct.stacks = -1;
-        input_struct.outputFile = tokens[4];
-    }
-    return input_struct;
-}
-
 void executeCommands(GeometricFigure input_struct) {
 
     GLUquadric* quad = gluNewQuadric();
@@ -126,8 +94,8 @@ int main(int argc, char ** argv) {
 	GeometricFigure * gf = new GeometricFigure;
 	
 	try {
-		gf->geometric_type = get_geometric_type(argv[argc-1]);
-		parse_values(gf, argc-2, argv);
+		gf->geometric_type = getGeometricType(argv[argc-1]);
+		parseValues(gf, argc-2, argv);
 	}
 	catch (exception &ex) {
 		cout << ex.what() << endl;
@@ -135,26 +103,6 @@ int main(int argc, char ** argv) {
 	}
 	
 	cout << "GF: " << gf << endl;
-	
-	/*for (int i = argc-2; i > 0; i--) {
-		gf->values.push_back(stoi(argv[i]));
-	}
-	
-	for (int k : gf->values)
-		cout << "N: " << k << endl;
-	
-	for (int i = argc-2; i > 0; i--) {
-			
-		cout << "N:" << i << " Text: " << argv[i] << endl; 
-	}
-	string commands = "generator sphere 1 10 10 sphere.3d";
-    GeometricFigure input = parseThreeCoordenateInput(commands);
-    executeCommands(input);
-    cout << "Graphic type: " << input.graphicType << '\n';
-    cout << "radius_or_length: " << input.radius_or_length << '\n';
-    cout << "slices_or_grid: " << input.slices_or_grid << '\n';
-    cout << "Stacks: " << input.stacks << '\n';
-    cout << "Output file: " << input.outputFile << '\n';*/
-
+	//executeCommands(...);
     return 0;
 }
