8493ddca0ccb8d24556da28f2ad0c32528041c30
jcbraz <josecbraz@outlook.com>
2023-04-07 19:11:15 +0100

    XML logic made simpler

---
+++ src/build/_deps/tinyxml2-build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /Users/jcbraz/University/3A2S/CG/CG-project/src/build/_deps/tinyxml2-src
+# Build directory: /Users/jcbraz/University/3A2S/CG/CG-project/src/build/_deps/tinyxml2-build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(xmltest "/Users/jcbraz/University/3A2S/CG/CG-project/src/build/_deps/tinyxml2-build/xmltest")
+set_tests_properties(xmltest PROPERTIES  PASS_REGULAR_EXPRESSION ", Fail 0" WORKING_DIRECTORY "/Users/jcbraz/University/3A2S/CG/CG-project/src/build/_deps/tinyxml2-src" _BACKTRACE_TRIPLES "/Users/jcbraz/University/3A2S/CG/CG-project/src/build/_deps/tinyxml2-src/CMakeLists.txt;48;add_test;/Users/jcbraz/University/3A2S/CG/CG-project/src/build/_deps/tinyxml2-src/CMakeLists.txt;0;")
