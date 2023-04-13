53e4225cb7e6d2c18c6beb1e8b71c65614f49905
jcbraz <josecbraz@outlook.com>
2023-04-13 15:53:00 +0100

    XML

--- src/build/_deps/tinyxml2-build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/user13/Desktop/Github/CG-project/src/build/_deps/tinyxml2-src
-# Build directory: /home/user13/Desktop/Github/CG-project/src/build/_deps/tinyxml2-build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(xmltest "/home/user13/Desktop/Github/CG-project/src/build/_deps/tinyxml2-build/xmltest")
-set_tests_properties(xmltest PROPERTIES  PASS_REGULAR_EXPRESSION ", Fail 0" WORKING_DIRECTORY "/home/user13/Desktop/Github/CG-project/src/build/_deps/tinyxml2-src" _BACKTRACE_TRIPLES "/home/user13/Desktop/Github/CG-project/src/build/_deps/tinyxml2-src/CMakeLists.txt;48;add_test;/home/user13/Desktop/Github/CG-project/src/build/_deps/tinyxml2-src/CMakeLists.txt;0;")
